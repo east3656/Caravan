ff2b4a23aaadcc04860963fe2ac2334663b8c40e
Manshen He <80552756+east3656@users.noreply.github.com>
2024-11-24 21:46:42 -0500

    moved Card.java

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -1,1 +1,37 @@
+public class Card {
+    public enum Rank {
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
 
+    public enum Suit {
+        HEARTS, DIAMONDS, CLUBS, SPADES
+    }
+
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+    /*
+    public static void main(String[] args) {
+        Card card = new Card(Rank.ACE, Suit.SPADES);
+        System.out.println(card);
+    }
+*/
+}
