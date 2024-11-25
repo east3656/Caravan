3896ef06bedc9349bf25b7034e63e18396432913
Manshen He <80552756+east3656@users.noreply.github.com>
2024-11-25 13:15:08 -0500

    Update GameModel.java

--- src/main/model/GameModel.java
+++ src/main/model/GameModel.java
@@ -1,5 +1,15 @@
+package model;
+
 public interface GameModel {
     void initializeGame();
+
+    //Actions
+    void playCard();
+    void playCardOpp();
+
+    
+
+    
     boolean isGameOver();
     void playTurn(Player player);
     void endGame();
