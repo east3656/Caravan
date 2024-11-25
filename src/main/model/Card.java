ac008da5d6e4e00583ea59567d241d2c5ae8d971
Manshen He <80552756+east3656@users.noreply.github.com>
2024-11-25 12:55:12 -0500

    Update Card.java to Concrete type PokerCard

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -1,4 +1,4 @@
-public class Card {
+public class PokerCard implements Card{
     public enum Rank {
         TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
     }
@@ -10,7 +10,7 @@ public class Card {
     private final Rank rank;
     private final Suit suit;
 
-    public Card(Rank rank, Suit suit) {
+    public PokerCard(Rank rank, Suit suit) {
         this.rank = rank;
         this.suit = suit;
     }
@@ -23,12 +23,15 @@ public class Card {
         return suit;
     }
 
+    
+    
+    /*
     @Override
     public String toString() {
         return rank + " of " + suit;
     }
 
-    /*
+    
     public static void main(String[] args) {
         Card card = new Card(Rank.ACE, Suit.SPADES);
         System.out.println(card);
