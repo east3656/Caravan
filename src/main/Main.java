55bc35781571b65bc3133e20ed91592ef1585841
Manshen He <80552756+east3656@users.noreply.github.com>
2024-11-26 22:14:54 -0500

    Create Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import main.model.GameModel;
+import model.CaravanGameModel;
+
+public class Main {
+    public static void main(String[] args) {
+        CaravanGameModel gameModel = new CaravanGameModel();
+
+        gameModel.startGame();
+    }
+}
